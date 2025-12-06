0e623eba2bc00843ce8646a041a61a3603d056c3
Emanuel Julio Lima Gomes <salsichabrasileiro04@gmail.com>
2025-12-06 08:50:37 -0300

    Revert "Update script.js"
    This reverts commit 44b65790c253360c76943ad8aee8b689059bfd29.

--- script/script.js
+++ script/script.js
@@ -68,7 +68,7 @@ map.on("click", (e) => {
                 pinTemporario = null;
                 denunciaAtiva = false;
                 btnDenuncia.textContent = "Denúncia: OFF";
-                btnDenuncia.className = "btn-toggle desativado btn-toggle";
+                btnDenuncia.className = "btn-toggle desativado";
             };
         }
 
@@ -102,7 +102,7 @@ map.on("click", (e) => {
                     }
                     denunciaAtiva = false;
                     btnDenuncia.textContent = "Denúncia: OFF";
-                    btnDenuncia.className = "btn-toggle desativado btn-toggle";
+                    btnDenuncia.className = "btn-toggle desativado";
                 })
                 .catch(err => {
                     alert("Erro na requisição: " + err);
